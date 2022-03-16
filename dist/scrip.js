62c8c62dc06a8452dc895912d72e47b1ccf2ad3d
Luciana Santos <luciydss@hotmail.com>
2022-03-15 22:15:02 -0300

    Menu mobile finalizado

--- dist/scrip.js
+++ dist/scrip.js
@@ -1,2 +1,2 @@
-"use strict";console.log("Teste");var test=function(){console.log("Outro teste")};
+"use strict";var menu=document.querySelector("#menu"),menuIcon=document.querySelector("#menu-icon"),body=document.querySelector("body");function abrirMenu(){menu.classList.toggle("ativo"),body.classList.toggle("no-scroll")}menuIcon.addEventListener("click",abrirMenu);
 //# sourceMappingURL=scrip.js.map
